8191f8cdf3bd1949c908d5dad3cc67b4e2467afd
SHIMA RYUHEI <65934663+islandryu@users.noreply.github.com>
2023-11-08 12:09:58 +0900

    firstcommit

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity
+ } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import Animated, {
+  useAnimatedScrollHandler,
+  useSharedValue,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
 
 export default function App() {
+  const scrollY = useSharedValue(0);
+
+  const onScroll = useAnimatedScrollHandler((event) => {
+    scrollY.value = event.contentOffset.y;
+  }, []);
+
+  const scrollViewStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: 300 -scrollY.value }],
+      width: 300,
+    };
+  });
+
+  const contentStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: scrollY.value }],
+    };
+  },[]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.ScrollView 
+        onScroll={onScroll}
+        scrollEventThrottle={16}
+        style={scrollViewStyle}
+      >
+        <Animated.View style={[{ height: 5000, backgroundColor: 'red' }, contentStyle]} >
+          {Array.from({ length: 100 }).map((_, i) => (
+            <Text key={i}>{i}</Text>
+          ))}
+        </Animated.View>
+      </Animated.ScrollView>
     </View>
   );
 }
@@ -18,3 +52,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
